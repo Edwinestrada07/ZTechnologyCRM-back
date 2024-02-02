a06637b0d65fad5641a228afe531570180cc74e9
Edwin Estrada <r.1993andres.ee@gmail.com>
2024-02-02 00:15:41 -0500

    Se crea el endpoint POST para el registro de users

--- auth/auth.js
+++ auth/auth.js
@@ -4,6 +4,7 @@ import User from '../user/user.model.js'
 
 const app = Express.Router() 
 
+//Ruta para iniciar sesión
 app.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({
@@ -30,4 +31,36 @@ app.post('/login', async (req, res) => {
     
 })
 
+// Ruta para el registro de usuarios
+app.post('/signup', async (req, res) => {
+    try {
+      const existingUser = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+  
+      if (existingUser) {
+        return res.status(400).send('El usuario ya existe');
+      }
+  
+      const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password, // Asegúrate de agregar lógica de hash aquí
+        role: req.body.role || 'ADMIN',
+      });
+  
+      const token = jwt.sign({ id: newUser.id }, '3de113c0-757c-45be-a5ab-238221699cd2', {
+        expiresIn: '2h',
+      });
+  
+      res.status(201).send({ token, user: { ...newUser.dataValues } });
+  
+    } catch (error) {
+      console.error('Error al intentar registrar usuario:', error);
+      res.status(500).send('Error interno del servidor');
+    }
+});
+  
 export default app; 
