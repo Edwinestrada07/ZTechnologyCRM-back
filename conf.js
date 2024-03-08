ad50e56afebbe5ee0ee1f0e79f04aef1883ade29
Edwin Estrada <r.1993andres.ee@gmail.com>
2024-03-07 19:10:21 -0500

    Integration conf.js

---
+++ conf.js
@@ -0,0 +1,4 @@
+// config.js
+export const NODE_ENV = process.env.NODE_ENV || 'store-enyoi-node';
+export const HOST = process.env.HOST || '192.168.116.153:3000';
+export const PORT = process.env.PORT || 4000;
